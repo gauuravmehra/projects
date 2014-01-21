1adc0683b9010ee57ea729d68dda20f263ba1076
gauuravmehra <gauurav.mehra@gmail.com>
2014-01-21 23:05:24 +0530

    changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,25 +5,14 @@ module.exports = function(grunt) {
     grunt.initConfig({
         //pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-             dist: {
-				src: [
-					'js/libs/*.js', // All JS in the libs folder
-					'js/global.js'  // This specific file
-				],
-				dest: 'js/build/production.js',
-			}
-        },
-		
-		less: {
+        less: {
 			options: {
 			  compress: true,
 			  yuicompress: true
 			},
 			development: {
 				files: {
-					"css/media.css": "less/media.less",
-				  "css/signin-phone.css": "less/signin-phone.less"
+					"css/app.css": "less/app.less"
 				}
 			}
 		},
@@ -41,12 +30,10 @@ module.exports = function(grunt) {
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     //grunt.registerTask('default', ['concat']);
 	grunt.registerTask('watch-less', ['watch']);
-
 };
